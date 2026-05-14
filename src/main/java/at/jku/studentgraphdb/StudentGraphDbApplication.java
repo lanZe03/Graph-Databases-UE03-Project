8ed1b06246d31c0b2f34de771054145423b2961e
lanZe03 <110919679+lanZe03@users.noreply.github.com>
2026-05-14 18:12:53 +0200

    Starter Setup with Spring initializer

---
+++ src/main/java/at/jku/studentgraphdb/StudentGraphDbApplication.java
@@ -0,0 +1,13 @@
+package at.jku.studentgraphdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGraphDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentGraphDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/jku/studentgraphdb/StudentGraphDbApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jku.studentgraphdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGraphDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
